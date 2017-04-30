3a55d659d955d4fe25c0c8d5625efd94dd7afa57
Tiago Hellebrandt Silva <thellebrandtsilva@gmail.com>
2017-04-30 16:35:26 -0400

    Definindo idioma e charset

--- langpy.py
+++ langpy.py
@@ -1,9 +1,12 @@
 # Gerencia idiomas em uma aplicação Python.
 try:
     import json
+    import locale
 except ImportError as err:
     print(err)
 class langpy:
 
     def __init__(self):
         self.path = "langpy.txt" # Caminho do dicionário.
+        self.lang = locale.getdefaultlocale()[0]
+        self.charset = locale.getdefaultlocale()[1]
